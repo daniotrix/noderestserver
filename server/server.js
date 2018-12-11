588c009c6699bb69a3a02ad51efbb4579a482b76
DanielGaleana <daniotrix@gmail.com>
2018-12-11 01:48:09 -0600

    primer commit

---
+++ server/server.js
@@ -0,0 +1,22 @@
+require('./config/config')
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.use(require('./rutas/usuario'));
+
+mongoose.connect('mongodb://localhost:27017/asdasd', (error) => {
+    if (error) throw error;
+    console.log('Base de datos online');
+});
+
+app.listen(process.env.PORT, () => {
+    console.log('Escuchando el puerto: ', 3000);
+})
